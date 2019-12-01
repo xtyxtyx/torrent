3917419c51a3a5340e567eba7cd005b23a076545
xuty <xty50337@hotmail.com>
2019-12-01 22:23:00 +0800

    add codec for bencode

--- lib/src/bencode/bencode.dart
+++ lib/src/bencode/bencode.dart
@@ -1,4 +1,24 @@
 library bencode;
 
+import 'dart:convert';
+
+import 'package:torrent/src/bencode/bdecoder.dart';
+import 'package:torrent/src/bencode/bencoder.dart';
+
 export 'bencoder.dart';
 export 'bdecoder.dart';
+
+class BencCodec extends Codec<Object, List<int>> {
+
+  const BencCodec();
+
+  dynamic decode(List<int> source) => bDecoder.convert(source);
+
+  List<int> encode(Object value) => bEncoder.convert(value);
+
+  BEncoder get encoder => bEncoder;
+
+  BDecoder get decoder => bDecoder;
+}
+
+final benc = BencCodec();
